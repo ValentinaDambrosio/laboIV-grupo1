88864d980bab15b752547de698ef06f703d7b962
SebasF1349 <ratablanca711@gmail.com>
2024-09-15 19:40:59 -0300

    fix: realizar pronostico para los próximoz 7 dias

--- controllers/pronostico.js
+++ controllers/pronostico.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const getPronostico = (req, res) => {
   const bbLatitude = '-38.7176'
   const bbLongitude = '-62.26545'
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${bbLatitude}&longitude=${bbLongitude}&hourly=temperature_2m,apparent_temperature,precipitation_probability,rain&timezone=America%2FSao_Paulo&forecast_days=1`
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${bbLatitude}&longitude=${bbLongitude}&hourly=temperature_2m,apparent_temperature,precipitation_probability,rain&timezone=America%2FSao_Paulo`
   axios
     .get(url)
     .then((response) => {
